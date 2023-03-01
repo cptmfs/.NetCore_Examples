f6c71c3783f1ec53d44500eed68e0ebed9340071
Muhammed Ferit Simsek <cpt.mfs@gmail.com>
2023-03-01 23:37:33 +0300

    .

--- NightlyWorkout/02_HesapMakinesi/Program.cs
+++ NightlyWorkout/02_HesapMakinesi/Program.cs
@@ -4,16 +4,9 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        //Kullanıcıdan alınan 2 sayıya yine kullanıcıdan alınacak işlem seçimine göre dört işlem uygulayan program kodunu yazınız. (Toplama için T veya t, Çıkartma için E/e, Çarpma için Ç/ç, Bölme için B/b kodlarını kullanınız. Eğer bu işlem kodları dışındaki girişler için hatalı giriş yapıldığına dair bir mesaj verdirebilirsiniz)
+        //4 İşlem yapabilen Calculator Yapınız..
 
-        //    int sayi1, sayi2;
-        //    string girilenDeger;
 
-        //    Console.WriteLine("Lütfen sayi1'i giriniz : ");
-        //    sayi1 = Int32.Parse(Console.ReadLine());
-        //    Console.WriteLine("Lütfen sayi2'yi giriniz : ");
-        //    sayi2 = Int32.Parse(Console.ReadLine());
-        //adim1:
         CalculateManager calculateManager = new CalculateManager();
         Console.WriteLine("\t\t---------------------Hesap Makinesi---------------------");
         Console.WriteLine("\n\nBilgisayar 1 ile 200 arası 2 adet rastgele sayı oluşturdu.\n");
@@ -25,19 +18,9 @@ internal class Program
         Console.WriteLine($"\nİkinci Sayı : {hMakine.sayi2}" );
 
         Console.Write("\nBölme İşlemi için :b \nÇarpma İşlemi için :ç \nToplama İşlemi için :t \nÇıkarma İşlemi için :e tuşlayınız..\nLütfen yapmak istediğiniz işlemi tuşlayınız : ");
-       
-        
         hMakine.girilenDeger = Console.ReadLine();
         
-        
-
-
-
         calculateManager.Calculate4(hMakine);
-
-
-
-
 
         Console.ReadKey();
     }
